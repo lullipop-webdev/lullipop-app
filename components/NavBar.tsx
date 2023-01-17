5d67957bc002c4efe909e1513ba8f67ec0bdec89
lullipopco <116914101+lullipopco@users.noreply.github.com>
2023-01-17 14:12:09 +0000

    inti commit

---
+++ components/NavBar.tsx
@@ -0,0 +1,14 @@
+
+
+function NavBar() {
+  return (
+    <div className='flex grid-cols-3 gap-3 justify-start p-4'>
+      <div className="">Lullipop</div>
+      <div className="">Shop</div>
+      <div className="">Lullifit</div>
+
+    </div>
+  )
+}
+
+export default NavBar
