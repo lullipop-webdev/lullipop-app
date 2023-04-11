9eec7a2b43b9862ce3bc5cfbfc7525bd2d57ffef
Gerardo <g.socias29@gmail.com>
2023-04-11 22:21:27 +0800

    added slider images from collections

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react';
+import { getCollections } from '../lib/Shopify';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css'
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Navigation, Keyboard } from 'swiper';
+import { Autoplay, Navigation, Keyboard } from 'swiper';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -12,42 +14,51 @@ type Props = {
 }
 
 const Slider: React.FC<Props> = ({heading, message}) => {
+
+    const [data, setData] = useState([]);
+    const slider = [];
+    useEffect(() => {
+        getCollections().then((data) => {
+            setData(data);
+            console.log("data::", data);
+        });
+        
+    }, []);
+
     return (
         <Swiper
-        slidesPerView={1}
-        className="h-full w-full"
-        keyboard={{
-            enabled: true,
+            loop={true}
+            slidesPerView={1}
+            className="h-full w-full"
+            keyboard={{
+                enabled: true,
             }}
-        modules={[Navigation, Keyboard]}
-        navigation={true}
+            autoplay={{
+                delay: 5000,
+                disableOnInteraction: false,
+            }}
+            modules={[Autoplay, Navigation, Keyboard]}
+            navigation={true}
         >
         <div className="h-full w-full">
-            <SwiperSlide>
-                <div className='flex flex-col h-screen items-center justify-center mb-12 min-h-full'>
-                    <Image src="https://storage.googleapis.com/lullipop-image-dump/003%20Petra%20Photo%20Shoots/Lullipop_Jeans/JEZ_1642.jpg" alt='lullipop' className='object-cover' fill/>
-                    {/* <div className='flex'>
-                        <div className='flex flex-row p-5 text-white z-[2] mt-[6rem] space-x-56'>
-                            <p className='py-5 text-xs w-3 '>Trending Collection</p>
-                            <h1 className='text-6xl font-bold tracking-widest'>{heading}</h1>
-                        </div>  
-                    </div> */}
-                    <div className='flex flex-row mt-[10rem] z-[2] items-center justify-between w-full'>
-                        <div className='w-80'>
+            {
+                data.map((d, i)=>{
+                    return <SwiperSlide key={i}>
+                        <div className='flex flex-col h-screen items-center justify-center mb-12 min-h-full'>
+                            <Image src={d.node.image.url} alt={(d.node.image.altText != null) ? d.node.image.altText : d.node.title} className='object-cover' fill/>
+                            <div className='flex flex-row mt-[10rem] z-[2] items-center justify-between w-full'>
+                                <div className='w-80'></div>
+                                <div className='z-[2]'>
+                                    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl text-gray-500 dark:text-white text-center">{d.node.title}</h1>
+                                    <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-white text-center">{d.node.description}</p>
 
-                        </div>
-                        <div className='z-[2]'>
-                            <Link href="/products">
-                                <button className='px-8 py-2 border-2 border-pink-400 rounded-3xl text-white'>View Products</button>
-                            </Link>
-                        </div>
-                        <div className='flex flex-row p-5 text-white z-[2] mt-[-0.5rem] w-80'>
-                            {/* <h1 className='text-sm font-bold text-black'>Info</h1>
-                            <p className='py-5 text-xs text-black'>{message}</p> */}
-                        </div>
-                    </div>
-                </div>    
-            </SwiperSlide>
+                                </div>
+                                <div className='flex flex-row p-5 text-white z-[2] mt-[-0.5rem] w-80'></div>
+                            </div>
+                        </div>    
+                    </SwiperSlide>
+                })
+            }
         </div>
     </Swiper>
     )
