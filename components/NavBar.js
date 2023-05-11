7eae3ffe5b89630453154fe024bb548bdf7225ad
Gerardo <g.socias29@gmail.com>
2023-05-12 00:06:56 +0800

    fix: Menu should not open full screen

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,8 @@
 import Link from 'next/link';
 import React, { Fragment, useState, useEffect, useContext } from 'react';
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
+
 import { CartContext } from '../context/shopContext';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { HiShoppingBag } from 'react-icons/hi'
@@ -10,7 +13,7 @@ import AccountIcon from '../assets/customer_acc_icons-03.png';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
 import { useRouter } from 'next/router';
-import { Menu, Transition } from '@headlessui/react'
+import { Menu, Transition, Dialog, Disclosure } from '@headlessui/react'
 import { useAuth } from '@/context/AuthContext';
 
 
@@ -180,25 +183,61 @@ export default function Navbar() {
         
         
         {/* Mobile Menu */}
-        <div
-          className={
-            nav
-              ? 'absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white dark:bg-black text-center ease-in duration-300'
-              : 'absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white dark:bg-black text-center ease-in duration-300'
-          }
-        >
-          <ul>
-            <li onClick={handleNav} className='p-2 text-2xl hover:text-gray-500'>
-              <Link href='/'>Lullipop</Link>
-            </li>
-            <li onClick={handleNav} className='p-2 text-2xl hover:text-gray-500'>
-              <Link href='/shop'>Shop</Link>
-            </li>
-            <li onClick={handleNav} className='p-2 text-2xl hover:text-gray-500'>
-              <Link href='/work'>Lullifit</Link>
-            </li>            
-          </ul>
-        </div>
+
+        <Transition.Root show={nav} as={Fragment}>
+          <Dialog as="div" className="relative z-40" onClose={setNav}>
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
+            <div className="fixed inset-0 z-40 flex">
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <Dialog.Panel className="relative flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
+                  <div className="flex ml-auto px-4">
+                    <button
+                      type="button"
+                      className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
+                      onClick={() => setNav(false)}
+                    >
+                      <span className="sr-only">Close menu</span>
+                      <XMarkIcon className="h-6 w-6 focus-outline-none" aria-hidden="true" />
+                    </button>
+                  </div>
+
+                  <Dialog.Panel className="pt-6">
+                    <ul className='px-6'>
+                      <li onClick={handleNav} className='p-2 text-2xl hover:text-gray-500'>
+                        <Link href='/'>Lullipop</Link>
+                      </li>
+                      <li onClick={handleNav} className='p-2 text-2xl hover:text-gray-500'>
+                        <Link href='/shop'>Shop</Link>
+                      </li>
+                      <li onClick={handleNav} className='p-2 text-2xl hover:text-gray-500'>
+                        <Link href='/work'>Lullifit</Link>
+                      </li>            
+                    </ul>
+                  </Dialog.Panel>
+
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition.Root>
       </div>
     </div>
   );
