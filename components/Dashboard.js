e213155cf9f8eb2a516c680ab3a1b56415ecefaa
fcocotech <franco@cocotechsolutions.com>
2023-06-25 07:17:30 +0800

    Milestone June 2023

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,20 +1,51 @@
-import  { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { getCustomerDetails,getCustomerOrders } from '../lib/Shopify';
+import { AuthChecker } from '../lib/AuthChecker';
+import { redirect } from 'next/navigation'
 
-import Sidebar from './Sidebar' 
-import { useAuth, ProtectRoute } from '@/context/AuthContext';
+export default function Dashboard() {
 
-export default function Dashboard({children}) {
-    const router = useRouter();
-    const {user, isAuthenticated} = useAuth();
+    const [data, setData] = useState(null);
+    const [customerOrders, setCustomerOrders] = useState(null);
 
+    useEffect(() => {
+        const accessToken = localStorage.getItem('accessToken');
+
+        getCustomerDetails(accessToken).then((data) => {
+            setData(data)
+        })
+
+        getCustomerOrders(accessToken).then((data) => {
+            setCustomerOrders(data)
+        })
+        
+    }, [])
+
+    console.log(customerOrders)
     return (
-        <ProtectRoute>
-            <div className=" min-h-fit flex flex-row justify-start mx-2 xl:ml-32 ">
-                <Sidebar customer={user} />
-                <div className="bg-primary flex-1 p-4 text-white">
-                    {children}
-                </div>
+        <div className="bg-white dark:bg-black">
+            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <h2 className="mt-12 text-2xl font-extrabold text-black dark:text-white mb-6">
+                Your Orders {data?.email}
+            </h2>
+            <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                {/* {
+                products?.map(product => (
+                    <ProductCard key={product.node.id} product={product} />
+                ))
+                
+                } */}
+                
+                {
+                    (customerOrders !== null) ? customerOrders[0].node.lineItems.edges.map((order) => {
+                        <h1>{order.node.title}</h1>
+                    }) : console.log("false")
+                }
+                
+    
             </div>
-        </ProtectRoute>
+            </div>
+        </div>
     )
+    
 }
